202e8cfc30a4217163de4aa59e65401efb76cbf5
Cloudwalker <zeljkovranic@gmail.com>
2021-12-27 15:18:57 +0100

    111

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create();
+const http = require("http");
+var server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/"));
+console.log(__dirname);
 
-app.set("views", "./views");
+app.set("views", "./src/views");
 
 app.get("/", function (req, res) {
   res.render("home", { layout: false });
 });
 
-app.listen(8080);
+server.listen(8080);
